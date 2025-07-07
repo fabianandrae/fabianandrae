a1c489eec8a8a4f44b9e387f8d744e7d63a380e4
Fabian Andrä <fandrae@stud.hs-heilbronn.de>
2025-07-07 22:34:47 +0200

    Git Submodul entfernt

---
+++ showcase-site/gatsby-config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Fabian Andrä - Software Engineer`,
+    siteUrl: `https://fabianandrae.github.io`,
+  },
+  plugins: [
+    'gatsby-plugin-postcss',
+  ],
+}
